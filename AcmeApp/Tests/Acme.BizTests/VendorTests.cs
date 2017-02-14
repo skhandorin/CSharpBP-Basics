109e5f08531a780ff924ad9f4a7457701ff0c28c
skhandorin <hsatig@mail.ru>
2017-02-14 17:30:50 +0500

    After Creating Good Methods - Method Chaining

--- AcmeApp/Tests/Acme.BizTests/VendorTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
 
 namespace Acme.Biz.Tests
 {
@@ -54,6 +55,45 @@ namespace Acme.Biz.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true, "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12");
+
+            var actual = vendor.PlaceOrder(product, 12);
+
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        public void PlaceOrder_3Parameters()
+        {
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true, "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12\r\nDeliver By: 25.10.2017");
+
+            var actual = vendor.PlaceOrder(product, 12,
+                new DateTimeOffset(2017, 10, 25, 0, 0, 0, new TimeSpan(5, 0, 0)));
+
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrder_NullProduct_Esception()
+        {
+            var vendor = new Vendor();
+
+            var actual = vendor.PlaceOrder(null, 12);
+
+            // Assert
+            // Expected exception
         }
     }
 }
